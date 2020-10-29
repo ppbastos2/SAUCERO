bc6259a3cd2bd5766952815f9876ab48af36d3c6
kakog <kakogb@hotmail.com>
2020-10-29 16:23:27 -0300

    Inicialização do HTML

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Saucero</title>
+</head>
+<body>
+    <header>
+        <div>
+            <img src="" alt="">
+            <div>
+                <span>(11) 97530-7935</span>
+                <span>9:00 - 18:00</span>
+            </div>
+        </div>
+        <div>
+            <img src="" alt="Saucero">
+
+        </div>
+        
+        <div>
+            <div>
+                <span>R$0,00</span>
+                <span>0 itens</span>
+            </div>
+        </div>
+    </header>
+
+
+
+
+</body>
+</html>
