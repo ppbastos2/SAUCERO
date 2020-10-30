41d7626b3af57101b1cfe6179c4b00635e0276b1
kakog <kakogb@hotmail.com>
2020-10-30 09:36:49 -0300

    Marcação html ok

--- index.php
+++ index.php
@@ -16,7 +16,15 @@
         </div>
         <div>
             <img src="" alt="Saucero">
-
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Menu</a></li>
+                    <li><a href="">Features</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </nav>
         </div>
         
         <div>
@@ -27,7 +35,238 @@
         </div>
     </header>
 
+    <main>
+        <section>
+            <div>
+                <article>
+                    <h2>Vincent Pizza 2</h2>
+                    <h3>making people happy</h3>
 
+                    <footer>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+
+                        <a href="">Book now</a>
+                        <a href="">View Menu</a>
+                    </footer>
+                </article>
+                <div>
+                    <img src="" alt="banner">
+                </div>
+            </div>
+            <div>
+                    <a href="" title="previous">Anterior</a>
+                    <a href="" title="previous">Próximo</a>
+            </div>
+        </section>
+
+        <section>
+            <div>
+                <article>
+                    <img src="" alt="Quality Foods">
+                    <h2>Quality Foods</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </article>
+                <article>  
+                    <img src="" alt="Quality Foods">
+                    <h2>Quality Foods</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                </article>
+                <article>
+                    <img src="" alt="Quality Foods">
+                    <h2>Quality Foods</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </article>
+            </div>
+        </section>
+
+        <section>
+            <header>
+                <div>
+                    <article>
+                        <img src="" alt="">   
+                        <h2>Diablo</h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+                        <p><span>R$20,00</span></p>
+                    </article>
+
+                    <article>
+                        <img src="" alt="">   
+                        <h2>Carbonara</h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+                        <p><span>R$20,00</span></p>
+                    </article>
+
+                    <article>
+                        <img src="" alt="">   
+                        <h2>Capricciosa</h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+                        <p><span>R$20,00</span></p>
+                    </article>
+
+                    <article>
+                        <img src="" alt="">   
+                        <h2>Prosciutto</h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+                        <p><span>R$20,00</span></p>
+                    </article>
+
+                </div>
+            </header>   
+        </section>
+
+        <section>
+            <header>
+                <h1>Discover our Menu</h1>
+            </header>
+            <div>
+                <ul>
+                    <li><a href="">Burgers</a></li>
+                    <li><a href="">Desserts</a></li>
+                    <li><a href="">Drinks</a></li>
+                    <li><a href="">Pasta</a></li>
+                    <li><a href="">Pizzas</a></li>
+                    <li><a href="">Salads</a></li>
+                </ul>
+
+                <article>
+                    <img src="" alt="">
+                    <h2>Lorem, ipsum dolor.</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    <p><span>R$20,00</span></p>
+                </article>
+
+                <article>
+                    <img src="" alt="">
+                    <h2>Lorem, ipsum dolor.</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    <p><span>R$20,00</span></p>
+                </article>
+
+                <article>
+                    <img src="" alt="">
+                    <h2>Lorem, ipsum dolor.</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    <p><span>R$20,00</span></p>
+                </article>
+
+                <article>
+                    <img src="" alt="">
+                    <h2>Lorem, ipsum dolor.</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    <p><span>R$20,00</span></p>
+                </article>
+
+                <article>
+                    <img src="" alt="">
+                    <h2>Lorem, ipsum dolor.</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    <p><span>R$20,00</span></p>
+                </article>
+
+                <article>
+                    <img src="" alt="">
+                    <h2>Lorem, ipsum dolor.</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    <p><span>R$20,00</span></p>
+                </article>
+                
+                <article>
+                    <img src="" alt="">
+                    <h2>Lorem, ipsum dolor.</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    <p><span>R$20,00</span></p>
+                </article>
+
+                <article>
+                    <img src="" alt="">
+                    <h2>Lorem, ipsum dolor.</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    <p><span>R$20,00</span></p>
+                </article>
+
+                <article>
+                    <img src="" alt="">
+                    <h2>Lorem, ipsum dolor.</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    <p><span>R$20,00</span></p>
+                </article>
+
+                <article>
+                    <img src="" alt="">
+                    <h2>Lorem, ipsum dolor.</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    <p><span>R$20,00</span></p>
+                </article>
+
+                <article>
+                    <img src="" alt="">
+                    <h2>Lorem, ipsum.</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    <p><span>R$20,00</span></p>
+                </article>
+
+                <article>
+                    <img src="" alt="">
+                    <h2>Lorem, ipsum.</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    <p><span>R$20,00</span></p>
+                </article>
+            </div>
+        </section>
+
+        <section>
+            <header>
+                <h1>Contact Us</h1>
+            </header>
+
+            <div>
+                <form action="">
+                    <div>
+                        <input type="text" name="name" placeholder="Seu Nome">
+                    </div>
+
+                    <div>
+                        <input type="email" name="email" placeholder="Seu EMail ">
+                    </div>
+
+                    <div>
+                        <input type="text" name="phone" placeholder="Seu Telefone">
+                    </div>
+
+                    <div>
+                        <textarea name="message" cols="30" rows="10"></textarea>
+                    </div>
+
+                    <button type="submit">Enviar Mensagem</button>
+                </form>
+            </div>
+        </section>
+    </main>
+
+    <div>
+        <a href="" title="Subir para o topo">Subir</a>
+    </div>
+
+    <footer>
+        <div>
+            <img src="" alt="Saucero">
+            <p>(11) 97530-7935 - 09:00 - 17:00</p>
+
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Menu</a></li>
+                <li><a href="">Features</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+
+            <ul>
+                <li><a href="">Facebook</a></li>
+                <li><a href="">Instagram</a></li>
+                <li><a href="">Twitter</a></li>
+            </ul>
+        </div>
+    </footer>
 
 
 </body>
