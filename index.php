9c3ea5d1f79d9a3b4f3c9141506edef3f5e40ce8
kakog <kakogb@hotmail.com>
2020-11-20 18:30:11 -0300

    html menu mobile e estilização do menu (mobile e desktop). e menu responsivo 992px

--- index.php
+++ index.php
@@ -37,9 +37,10 @@
     </div>
 
 <!-- END MODAL PRODUCTS -->
+
     <header class="__main_header">
         <div class="container">
-            <div class="row __main_header_content">
+            <div class="row __main_header_content __desktop">
             
                 <div class="row __main_header_content_contact">
                     <div class="icons">
@@ -75,6 +76,49 @@
                     </div>    
                 </div>
         
+            </div>
+
+            <button class="__btn_open_menu radius transition">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+
+            <div class="__main_header_mobile">
+                <div class="row __main_header_content_cart">
+                    
+                    <div class="cart">
+                        <a href="#" class="icon-bag"><span class="rounded">0</span></a>
+                    </div>      
+                
+                    <div class="price_items">
+                        <span>R$ 0,00</span>
+                        <a href="#">0 Itens - No Carrinho</a>
+                    </div>
+                      
+                </div>
+
+                <nav class="nav_list">
+                    <ul>
+                        <li class="active"><a href="">Home</a></li>
+                        <li><a href="">Menu</a></li>
+                        <li><a href="">Features</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ul>
+                </nav>
+
+                <div class="row __main_header_content_contact">
+                    <div class="icons">
+                        <span class="icon-mobile icon-notext"></span>
+                    </div>
+                    
+                    <div class="contacts">
+                        <span class="number">(11) 97530-7935</span>
+                        <span class="hours">9:00 - 18:00</span>
+                    </div>
+                </div>
+                
             </div>
         </div>
     </header>
